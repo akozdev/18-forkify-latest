6881b227af5b0b6021067882add5f8da3a3cb66b
Aleksander Kozłowski <aleksander.kozlowski@pm.me>
2024-08-20 23:03:43 +0200

    init

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c579098d-5f1f-4e7e-8230-628909ea4c8c';
+export const MODAL_CLOSE_SEC = 2.5;
